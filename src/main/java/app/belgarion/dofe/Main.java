18565e140c11eff0ea5e6763995a5e4c7094c679
= <=>
2026-02-15 14:29:59 +0100

    stuff added: - NumJavaArray#sort for 1 and 2 dimensional arrays - array constructor full of random numbers - the ability to add two of my arrays together - NumJavaArray#get: there are three methods, one simply where you put in the dimensions, e.g. 3,3 to get the third item in the third column
    stuff fixed:
    - NumJavaArray.arange: for two dimensions now actually returns in row-major as opposed to column-major
    
    todo:
    - implement NumJavaArray.arange for 3D
    - NumJavaArray#sort for 3D
    - NumJavaArray.concatenate for 3D

--- src/main/java/app/belgarion/dofe/Main.java
+++ src/main/java/app/belgarion/dofe/Main.java
@@ -5,10 +5,10 @@ package app.belgarion.dofe;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumJavaArray.NotImplementedException {
 
 
-        NumJavaArray test = NumJavaArray.createArray(new float[]{1,2,3,4,5,6});
+        NumJavaArray test = NumJavaArray.createArray(new float[]{5,9,4,3,5,6,86,5});
         System.out.println("---------");
         System.out.println("1 dimensional array");
         printDetails(test);
@@ -32,14 +32,30 @@ public class Main {
         printDetails(range1d);
         NumJavaArray range2d = NumJavaArray.arange(3,3);
         printDetails(range2d);
+        NumJavaArray rand1d = NumJavaArray.empty(3);
+        printDetails(rand1d);
+        NumJavaArray rand2d = NumJavaArray.empty(3,3);
+        printDetails(rand2d);
+        NumJavaArray rand3d = NumJavaArray.empty(3,3,3);
+        printDetails(rand3d);
+
+
+        NumJavaArray concat1dp1 = NumJavaArray.createArray(new float[]{1,2,3});
+        NumJavaArray concat1dp2 = NumJavaArray.createArray(new float[]{4,5,6});
+        printDetails(NumJavaArray.concatenate(concat1dp1, concat1dp2));
+        NumJavaArray concat2dp1 = NumJavaArray.createArray(new float[][]{new float[]{7,8,9}, new float[]{10,11,12}});
+        NumJavaArray concat2dp2 = NumJavaArray.createArray(new float[][]{new float[]{13,14,15}, new float[]{16,17,18}});
+        printDetails(NumJavaArray.concatenate(concat2dp1, concat2dp2));
     }
 
-    private static void printDetails(NumJavaArray range1d) {
-        System.out.println(range1d);
-        System.out.println("shape: "+ Arrays.toString(range1d.shape));
-        System.out.println("size: "+range1d.size);
-        System.out.println("ndim: " + range1d.ndim);
-        System.out.println("average: "+range1d.average());
+    private static void printDetails(NumJavaArray array) {
+        System.out.println(array);
+        System.out.println("shape: "+ Arrays.toString(array.shape));
+        System.out.println("size: "+array.size);
+        System.out.println("ndim: " + array.ndim);
+        System.out.println("average: "+array.average());
+        System.out.println("reversed: "+array.reversed());
+        System.out.println("sorted: "+array.sort());
         System.out.println("---------");
     }
 
--- src/main/java/app/belgarion/dofe/NumJavaArray.java
+++ src/main/java/app/belgarion/dofe/NumJavaArray.java
@@ -1,6 +1,8 @@
 package app.belgarion.dofe;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class NumJavaArray {
@@ -117,7 +119,7 @@ public class NumJavaArray {
             return new NumJavaArray(items);
         }
     }
-    public static NumJavaArray arange(int... params) {
+    public static NumJavaArray arange(int... params) throws NotImplementedException {
         if (params.length == 1) {
             IntStream items = IntStream.range(0, params[0]);
 
@@ -129,25 +131,63 @@ public class NumJavaArray {
             return new NumJavaArray(itemsArray);
 
         } else if (params.length == 2) {
-            // User will submit 2 items, length and width of matrix, for instance 3,3 returns [[1,2,3],[4,5,6],[7,8,9]]
-            int[] items = IntStream.range(0, params[0]*params[1]).toArray();
+            // params[0] = rows (length), params[1] = cols (width)
+
+            int[] items = IntStream.range(0, params[0] * params[1]).toArray();
             float[][] finalArray = new float[params[0]][params[1]];
-            for(int i=0; i<params[0];i++)
-                for(int j=0;j<params[1];j++)
-                    finalArray[i][j] = (float) items[(j*params[0]) + i];
+
+            for (int i = 0; i < params[0]; i++) {
+                for (int j = 0; j < params[1]; j++) {
+                    finalArray[i][j] = (float) items[(i * params[1]) + j];
+                }
+            }
+
             return new NumJavaArray(finalArray);
+
+        } else if (params.length == 3) {
+            throw new NotImplementedException("3 Dimensional array constructor of type range is not in place yet");
         }
         return null;
     }
+    public static NumJavaArray empty(int... params) {
+        Random random = new Random();
+        if (params.length == 1) {
+            float[] nums = new float[params[0]];
+            for (int i = 0; i < params[0]; i++) {
+                nums[i] = random.nextFloat();
+            }
+            return new NumJavaArray(nums);
+        } else if (params.length == 2) {
+            float[][] nums = new float[params[0]][params[1]];
+            for (int i = 0; i < params[0]; i++) {
+                for (int j = 0; j < params[1]; j++) {
+                    nums[i][j] = random.nextFloat();
+                }
+            }
+            return new NumJavaArray(nums);
+        } else {
+            float[][][] nums = new float[params[0]][params[1]][params[2]];
+            for (int i = 0; i < params[0]; i++) {
+                for (int j = 0; j < params[1]; j++) {
+                    for (int k = 0; k < params[2]; k++) {
+                        nums[i][j][k] = random.nextFloat();
+                    }
+                }
+            }
+            return new NumJavaArray(nums);
+        }
+    }
+
+
     public String toString() {
         return switch (ndim) {
             case 1 -> Arrays.toString(array1D);
-            case 2 -> this.deepToString(array2D);
+            case 2 -> NumJavaArray.deepToString(array2D);
             case 3 -> deeperToString(array3D);
             default -> throw new IllegalStateException("Unexpected value: " + ndim);
         };
     }
-    private String deepToString(float[][] items) {
+    private static String deepToString(float[][] items) {
         StringBuilder builder = new StringBuilder();
         builder.append('[');
         for (float[] arr : items) {
@@ -158,7 +198,7 @@ public class NumJavaArray {
         builder.append(']');
         return builder.toString();
     }
-    private String deeperToString(float[][][] items) {
+    private static String deeperToString(float[][][] items) {
         StringBuilder builder = new StringBuilder();
         builder.append('[');
 
@@ -194,6 +234,43 @@ public class NumJavaArray {
         }
         return list;
     }
+    public static NumJavaArray concatenate(NumJavaArray a, NumJavaArray b) throws NotImplementedException {
+        if (a.shape.length != b.shape.length) {
+            throw new IllegalArgumentException("The two arrays to concatenate must be of equal dimensions");
+
+        }
+        if (a.shape.length == 1) {
+            float[] merged = new float[a.array1D.length + b.array1D.length];
+
+            int idx = 0;
+            for (float n : a.array1D) merged[idx++] = n;
+            for (float n : b.array1D) merged[idx++] = n;
+            return new NumJavaArray(merged);
+        } else if (a.shape.length == 2) {
+            if (a.array2D[0].length != b.array2D[0].length)
+                throw new IllegalArgumentException(
+                        "column lengths must be the same with " +
+                                deepToString(a.array2D) + " and " + deepToString(b.array2D)
+                );
+
+            float[][] finalArray = new float[
+                    a.array2D.length + b.array2D.length
+                    ][a.array2D[0].length];
+
+            int index = 0;
+            for (int i = 0; i < a.array2D.length; i++) {
+                finalArray[index++] = a.array2D[i].clone();
+            }
+            for (int i = 0; i < b.array2D.length; i++) {
+                finalArray[index++] = b.array2D[i].clone();
+            }
+
+            return new NumJavaArray(finalArray);
+
+        } else {
+            throw new NotImplementedException("not done yet");
+        }
+    }
 
     public NumJavaArray reversed() {
 
@@ -227,16 +304,42 @@ public class NumJavaArray {
         }
         return null;
     }
-    public float get(int... indices) {
+
+
+    public NumJavaArray get(int... indices) {
         if (ndim != indices.length) throw new IllegalArgumentException("incorrect number of indices");
         if (ndim == 1) {
-            return array1D[indices[0]];
+            return new NumJavaArray(new float[]{array1D[indices[0]]});
         } else if (ndim == 2) {
-            return array2D[indices[0]][indices[1]];
+            return new NumJavaArray(new float[]{array2D[indices[0]][indices[1]]});
         } else {
-            return array3D[indices[0]][indices[1]][indices[2]];
+            return new NumJavaArray(new float[]{array3D[indices[0]][indices[1]][indices[2]]});
         }
     }
+    public NumJavaArray get(int index) {
+        if (array1D != null) {
+            return new NumJavaArray(new float[]{array1D[index]});
+        } else if (array2D != null) {
+            return new NumJavaArray(new float[]{array2D[index/array2D.length][index%array2D.length]});
+        } else if (array3D!=null) {
+            int x = index / (array3D[0].length * array3D[0][0].length );
+            int y = (index / array3D[0][0].length ) % array3D[0].length;
+            int z = index % array3D[0][0].length;
+            return new NumJavaArray(new float[]{array3D[x][y][z]});
+        } else return null;
+    }
+    public NumJavaArray get(int lowerbound, int upperbound) {
+
+        if (ndim == 1) {
+            float[] to_be_returned = new float[upperbound-lowerbound];
+            if (upperbound + 1 - lowerbound >= 0)
+                System.arraycopy(array1D, lowerbound, to_be_returned, 0, upperbound + 1 - lowerbound);
+            return new NumJavaArray(to_be_returned);
+        }
+        return null;
+    }
+
+
     public float average() {
         float total = 0;
         if (ndim==1) {
@@ -267,6 +370,21 @@ public class NumJavaArray {
             return total/(float) amountofNumbers;
         }
         return 0;
+    }
+    public NumJavaArray sort() {
+        if (array1D!=null) {
+            float[] temp = Arrays.copyOf(array1D, array1D.length);
+            Arrays.sort(temp);
+            return new NumJavaArray(temp);
+        } else if (array2D!=null) {
+            float[][] temp = Arrays.copyOf(array2D, array2D.length);
+            Arrays.sort(temp, Comparator.comparingDouble(o -> o[0]));
+            return new NumJavaArray(temp);
+        }
+        return null;
+    }
+    public static class NotImplementedException extends Exception {
+        private NotImplementedException(String message) {super(message);}
     }
 }
 
